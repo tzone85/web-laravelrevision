64219e495d7feb51abefa2f53b2384199facd524
thando <tzone85@gmail.com>
2018-03-25 01:44:59 +0200

    created a db, model and ran a migration script

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -4,14 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Message;
+
 class MessagesController extends Controller
 {
     public function submit(Request $request){
         $this->validate($request, [
-           'name' => 'required',
-           'email' => 'required'
+           'name' => 'required|min:3|alpha',
+           'email' => 'required|email|min:3'
         ]);
 
-        return 'SUCCESS';
+        // create a new message
+
+        $message = new Message;
+
+        $message->name = $request->input('name');
+        $message->email = $request->input('email');
+        $message->message = $request->input('message');
+
+        //save message
+        $message->save();
+
+        // redirect
+        return redirect('/');
     }
 }
